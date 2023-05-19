54aa59960ec6ad24499acd7dcc4a6aa9bcb4ad31
luizbearzi <94871271+luizbearzi@users.noreply.github.com>
2023-05-19 17:12:36 -0300

    finalizando pokedex

---
+++ script.js
@@ -0,0 +1,70 @@
+
+const types = [
+    'fire',
+    'grass',
+    'electric',
+    'water',
+    'ground',
+    'rock',
+    'fairy',
+    'poison',
+    'bug',
+    'dragon',
+    'psychic',
+    'flying',
+    'fighting',
+    'normal',
+]
+
+const POKEMON_COUNT = 12
+
+const cardHTMl = `
+<div class="card" id="card-{id}"> 
+<div class="title">
+    <h2>{name}</h2>
+    <small># {id}</small>
+</div>
+<div class="img bg-{type}">
+    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/{id}.png" alt="{name}">
+</div>
+<div class="type {type}">
+    <p>{type}</p>
+</div>
+<button class="favorite" data-id{id}>
+    <div class="heart">
+        
+    </div>
+</button>
+</div>
+`
+
+const getType = (data) => {
+    const apiTypes = data.map(type => type.type.name)
+    const type = types.find(type => apiTypes.indexOf(type ) > -1)
+    return type
+}
+
+const fetchPokemon = async (number) => {
+    if (number === undefined) return
+    const url = `https://pokeapi.co/api/v2/pokemon/${number}`
+    const response = await fetch(url).then(response => response.json())
+    const {id, name, types} = response
+    const type = getType(types)
+    return {id, name, type}
+}
+
+const replacer = (text, source, destination) => {
+   const regex = new RegExp()
+}
+
+const createPokemonCard = (pokemon) => {
+    
+}
+
+const fetchPokemons = async() => {
+    for (let i = 1; i <=  POKEMON_COUNT; i++) {
+        const pokemon = await fetchPokemon(i)
+    }
+}
+ 
+fetchPokemons()
